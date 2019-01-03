cd92d2e4af3eb8275f29f2415ee86982be5f15bd
Suzanne Richman <suzanne@favoredfortune.com>
2019-01-02 16:48:45 -0800

    more fun with functions in go

--- hello.go
+++ hello.go
@@ -9,8 +9,13 @@ import (
 	"math/rand"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 	fmt.Println("A fun number is", rand.Int())
 	fmt.Println("Who doesn't love", math.Pi, "?")
+	fmt.Println("Let's do some addition!", add(433, 226), "is 433 plus 226")
 }
